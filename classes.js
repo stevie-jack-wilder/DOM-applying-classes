799b0f55a9fa8631d8da1c29d6f7d5466d4c6fd1
stevie-jack-wilder <steviejwilder@gmail.com>
2020-07-11 19:40:19 +1000

    make div one green.

--- classes.js
+++ classes.js
@@ -6,23 +6,21 @@ document.addEventListener('DOMContentLoaded', start)
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
   one()
-    
+  
   // Your turn! Create a new function called `two`, then call it from here.
+  two()
 }
-
 function one () {
   // First, we have to find the element:
   var one = document.getElementById('one')
-
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
 }
-
 // CREATE FUNCTION two HERE
 function two () {
-  var two = document.getElementById('two')
+ var two = document.getElementById('two')
+ two.classList.add('green')
 }
-
 
 // CREATE FUNCTION three HERE
 
